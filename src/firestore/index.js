b7bcc74a47ceef2fed557f06f6ca45dc75607c91
Adam Cobb <cobb.2326@gmail.com>
2024-09-15 00:13:09 -0400

    Update index.js

--- src/firestore/index.js
+++ src/firestore/index.js
@@ -1,6 +1,6 @@
 
 import { isFirebaseAdminSDK, getLogger, optionalOptionsArg } from '#util/index.js'
-import { getFirestoreBase, encodeFirestoreData, decode } from '#firestore/util.js'
+import { getFirestoreBase, encodeFirestoreData } from '#firestore/util.js'
 import FirepowerDocSnap from '#firestore/FirepowerDocSnap.js'
 import FirepowerColSnap from '#firestore/FirepowerColSnap.js'
 
