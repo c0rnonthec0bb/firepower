82c94da6b9da325e0570c6f4726fddd34c0d007e
Adam Cobb <cobb.2326@gmail.com>
2024-09-14 21:13:54 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 
-export { setFirebaseBase, DataComparison, setLogger } from '#util/index.js'
+import { setFirebaseBase } from '#util/index.js'
+
+export { DataComparison, setLogger } from '#util/index.js'
 
 // where firebaseBase is from 'firebase-admin' or 'firebase/compat/app'
 export function initFirepower(firebaseBase) {
